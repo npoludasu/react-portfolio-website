21d90189f3025b96990fa5a6a7de6a90bea385d8
Naveen Poludasu <naveen.poludasu@infor.com>
2026-02-12 21:58:57 +0530

    Footer  updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="container mx-auto py-1 fixed bottom-0 md:left-20 bg-white dark:bg-dark-mode">
-      <p className="text-xs text-center text-dark-content dark:text-light-content w-full font-normal">
+      <p className="text-xs text-center text-dark-content dark:text-light-content w-full font-semibold">
         Designed and Built by{" "}
         <span className="font-semibold bg-gradient-to-r from-purple-700 via-pink-600 to-pink-700
           bg-clip-text text-transparent">Naveen Poludasu</span>
